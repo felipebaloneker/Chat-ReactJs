ed994447196b527dff026e7c990302a03b95c29b
hunter <hunter>
2022-03-02 21:59:04 -0300

    updated

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express()
+const cors = require('cors')
 const PORT = process.env.PORT || 3001
+const Base = process.env.REACT_APP_BASE_URL
+
+app.use(cors());
+require('dotenv').config()
 
 app.use(express.static('build'));
-app.listen(PORT, () => console.log(`Listening App!!`));
+app.listen(PORT, () => console.log(`Listening App!! Port: ${PORT} Base: ${Base}`));
--- src/services/database.ts
+++ src/services/database.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
-
 const database = axios.create({
-    baseURL:process.env.DATABASE_URL,
+    baseURL:process.env.REACT_APP_BASE_URL,
 })
 
 export default database;
