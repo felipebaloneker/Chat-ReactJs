de18afd89291a12f968baeb65bdfc4a35adb60ce
hunter <hunter>
2022-03-09 21:41:21 -0300

    updated server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express()
+const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 const PORT = process.env.PORT || 3001
 const Base = process.env.REACT_APP_BASE_URL
 
+app.use(cors)
 app.use(express.static('build'));
 app.listen(PORT, () => console.log(`Listening App!! Port: ${PORT} Base: ${Base}`));
